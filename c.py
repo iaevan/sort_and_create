5f92fe60dc9e6618c48247f51a7a8b41443eea63
Ishtiak Ahmed Evan <63555038+iaevan@users.noreply.github.com>
2020-08-16 15:32:34 +0600

    Add files via upload

---
+++ c.py
@@ -0,0 +1,9 @@
+with open("co.txt", "r") as c:
+    l = c.readlines()
+alp = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+for y in l:
+    for x in alp:
+        if x == y[0]:
+            with open((str(x)+".txt"), "a") as b:
+                b.write(y)
